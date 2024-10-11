b51f6c87b788e2874cc066ea7ca9b454aa052008
Manav Tomar <tmanav12@gmail.com>
2024-10-11 17:12:43 +0530

    second commit

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const ACTIONS = require('./src/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
+app.use(express.static('build'));  
+
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
